b69fdc76fe447816c2953cb36c9ae7f5ca7f1834
Stas Vereskovsky <vereskovskystas@gmail.com>
2019-02-18 00:55:59 +0300

    Implement detection algorithm

--- algorithm.rb
+++ algorithm.rb
@@ -1,7 +1,15 @@
 class Algorithm
-  EPS = 0.01
-  def self.compare_images(original, taken)
-    # cc.image.encode_image '.png'
-    raise NotImplementedError
+  EPS = 0.3
+
+  def self.compare_images(original, taken) # Returns true if images identical
+    counter = 0
+    image = original.abs_diff(taken)
+    (0...image.height).each do |height_count|
+      (0...image.width).each do |width_count|
+        pixel = image[height_count, width_count]
+        counter += 1 if pixel[0] <= 10 && pixel[1] <= 10 && pixel[2] <= 10
+      end
+    end
+    counter / (image.width * image.height).to_f > 1 - EPS
   end
 end
